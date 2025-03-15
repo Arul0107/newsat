89eda0ad74771a85c8340c429d802d4ebee5f4a7
Arul0107 <arulprakshv0107@gmail.com>
2025-03-16 02:04:11 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { Toaster } from "react-hot-toast";
 import AppLayout from "./components/sidebar/AppLayout";
 
-
 import Home from "./components/dashboard/Home";
 import About from "./components/Adduser/About";
 import Predict from "./components/profile/Predict";
-// import Team from "./components/userprev/Team";
 import Contact from "./components/report/Contact";
 import DetailOutput from "./components/userprev/DetailOutput";
 
@@ -17,22 +15,21 @@ const App = () => {
       <Toaster position="top-right" />
       <Router>
         <Routes>
-          {/* Default route redirecting to the dashboard */}
-          <Route path="/" element={<Navigate to="/" />} />
+          {/* Redirect root path to /home */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
 
-          <Route path="/*" element={
-            <AppLayout>
-              
-              <Routes>
-                <Route path="/home" element={<Home/>} />
-                <Route path="/about" element={<About />} />
-                <Route path="/prediction" element={<Predict />} />
-                <Route path="/team" element={<DetailOutput />} />
-                <Route path="/contact" element={<Contact />} />
-              </Routes>
-            </AppLayout>
-          } />
-        </Routes> 
+          {/* Nested routes under AppLayout */}
+          <Route path="/" element={<AppLayout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="prediction" element={<Predict />} />
+            <Route path="team" element={<DetailOutput />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+
+          {/* Catch-all for unknown routes */}
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
       </Router>
     </div>
   );
