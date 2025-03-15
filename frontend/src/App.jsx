04b6d5a8af4d14aedcf6c5b327eb0bbc40e7bfa8
Arul0107 <arulprakshv0107@gmail.com>
2025-03-16 01:47:06 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { Toaster } from "react-hot-toast";
 import AppLayout from "./components/sidebar/AppLayout";
 
+
 import Home from "./components/dashboard/Home";
 import About from "./components/Adduser/About";
 import Predict from "./components/profile/Predict";
+// import Team from "./components/userprev/Team";
 import Contact from "./components/report/Contact";
 import DetailOutput from "./components/userprev/DetailOutput";
 
@@ -15,21 +17,22 @@ const App = () => {
       <Toaster position="top-right" />
       <Router>
         <Routes>
-          {/* Redirect "/" to "/home" instead of itself */}
-          <Route path="/" element={<Navigate to="/home" replace />} />
+          {/* Default route redirecting to the dashboard */}
+          <Route path="/" element={<Navigate to="/" />} />
 
-          {/* Use AppLayout as a wrapper for all other pages */}
-          <Route path="/*" element={<AppLayout />}>
-            <Route path="home" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="prediction" element={<Predict />} />
-            <Route path="team" element={<DetailOutput />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-
-          {/* Catch-all route for 404 pages (optional) */}
-          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-        </Routes>
+          <Route path="/*" element={
+            <AppLayout>
+              
+              <Routes>
+                <Route path="/home" element={<Home/>} />
+                <Route path="/about" element={<About />} />
+                <Route path="/prediction" element={<Predict />} />
+                <Route path="/team" element={<DetailOutput />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </AppLayout>
+          } />
+        </Routes> 
       </Router>
     </div>
   );
